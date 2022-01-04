da3ccf5337fc02a6ab43a981ba6a84bf9129bff9
Alan <30361001+aljones1816@users.noreply.github.com>
2022-01-04 04:20:53 +0000

    major updates

--- JS/index.js
+++ JS/index.js
@@ -1,24 +1,93 @@
 const gameBoard = (() => {
-    const gameboard = ["X","O","X","O","X","O","X","O","X"];
-    const renderboard =  () => {
-        const gridSquares = ["one","two","three","four","five","six","seven","eight","nine"];
-        gridSquares.map(x => {
-            const square = document.getElementById(x);
-            const newText = document.createTextNode(gameboard[gridSquares.indexOf(x)]);
-            square.appendChild(newText);
+    const gameboard = [
+        {
+            id: "one",
+            value: "",
+            clicked:false
+        },
+        {
+            id: "two",
+            value: "",
+            clicked:false
+        },
+        {
+            id: "three",
+            value: "",
+            clicked:false
+        },
+        {
+            id: "four",
+            value: "",
+            clicked:false
+        },
+        {
+            id: "five",
+            value: "",
+            clicked:false
+        },
+        {
+            id: "six",
+            value: "",
+            clicked:false
+        },
+        {
+            id: "seven",
+            value: "",
+            clicked:false
+        },
+        {
+            id: "eight",
+            value: "",
+            clicked:false
+        },
+        {
+            id: "nine",
+            value: "",
+            clicked:false
+        },
+    ];
+
+    const renderboard =  (board) => {
+        board.map(x => {
+            const square = document.getElementById(x.id);
+            const newText = document.createTextNode(x.value);
+            square.replaceChildren(newText);
         })
     }
-    return {renderboard}
+
+    const getGameBoard = () => {
+        return gameboard;
+    }
+
+    const changeGameboard = (squareID, newValue) => {
+        gameBoard[gameBoard.indexOf(id => id == squareID)]
+    }
+    
+    return {renderboard, getGameBoard, changeGameboard}
 })();
 
-const gameFlow = (() => {
+const displayController = (() => {
+    let currentBoard = gameBoard.getGameBoard();
 
-    return {}
+    const markBoard = () => {
+        
+    }
 })();
 
-const player = () => {
+const createPlayer = (playerName,value,playerStatus) => {
+    const name = playerName;
+    const markerValue = value;
+    const status = playerStatus;
 
-    return {}
+    return {name, markerValue, status, markSquare}
 }
 
-gameBoard.renderboard();
+
+const gameFlow = (() => {
+    
+     
+    return {}
+})();
+
+gameBoard.changeGameboard("one","O");
+gameBoard.renderboard(gameBoard.getGameBoard());
