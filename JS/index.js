71a206b07273c91dae1a3c2e090deabaf2b37bc5
Alan <30361001+aljones1816@users.noreply.github.com>
2022-01-26 03:35:43 +0000

    added win condition function

--- JS/index.js
+++ JS/index.js
@@ -27,12 +27,12 @@ const gameBoard = (() => {
         },
         {
             id: "six",
-            value: "X",
+            value: "",
             clicked:false
         },
         {
             id: "seven",
-            value: "O",
+            value: "",
             clicked:false
         },
         {
@@ -57,31 +57,94 @@ const gameBoard = (() => {
 
     const changeGameboard = (squareID, player) => {
             let idToChange = gameboard.findIndex(square => square.id == squareID);
-            gameboard[idToChange].value = player.value;
+            if (gameboard[idToChange].clicked == false) {
+                gameboard[idToChange].value = player.value;
+                gameboard[idToChange].clicked = true;
+            }
             renderboard();
     }
     
-    return {renderboard, changeGameboard}
+    return {gameboard, renderboard, changeGameboard}
 })();
 
-const gameController = (() => {
-
-    //TODO implement a function that calls the updateGameboard method of the GameBoard 
-    // when a player clicks on a square
-
-    //TODO implement a function to check the victory condition of the board
-
-    //TODO implement a function to track the currently active player.
-
-})();
-
-const createPlayer = (playerName,value) => {
+const createPlayer = (playerName,markerValue,active) => {
     const name = playerName;
-    const markerValue = value;
-    return {name, markerValue}
+    const value = markerValue;
+    const isActive = active;
+    return {name, value, isActive}
 }
 
+const gameController = (() => {
+    const players = [
+        createPlayer("playerOne","X",true),
+        createPlayer("playerTwo","O",false)
+    ]
 
+
+    //Calls the changeGameboard method of the GameBoard 
+    // when a player clicks on a square
+    const boardSquares = document.querySelectorAll('.board-square')
+    
+    boardSquares.forEach(square => {
+        square.addEventListener('click', (e) => {
+            players.forEach(player => {
+                if (player.isActive) {
+                    gameBoard.changeGameboard(square.id,player)
+                }
+            })
+            checkWin(gameBoard.gameboard);
+            controlActivePlayer();
+            
+        })
+    });
+
+    //Check the victory condition of the board
+    const checkWin = (gameboard) => {
+
+        const winningPlays = [
+            ["one","two","three"],
+            ["four","five","six"],
+            ["seven","eight","nine"],
+            ["one","four","seven"],
+            ["two","five","six"],
+            ["three","six","nine"],
+            ["one","five","nine"],
+            ["seven","five","three"]
+        ];
+
+        const activePlayer = players.filter(player => {
+            return player.isActive
+          })
+
+        const activePlayerSquares = gameboard.filter(square => {
+            return square.value == activePlayer[0].value;
+        })  
+
+        const activePlayerSquareIds = activePlayerSquares.map(square => square.id);
+
+        winningPlays.forEach(play => {
+            if (play.every(elem => activePlayerSquareIds.indexOf(elem) > -1)) {
+                console.log(activePlayer[0].name + "wins!")
+            }
+        })
+
+
+    }
+
+    //Track the currently active player.
+    const controlActivePlayer = () => {
+        players.forEach(player => {
+            player.isActive = !player.isActive;
+        })
+    }
+
+})();
+
+const initializeGame = () => {
+    gameBoard.renderboard();
+    
+}
 // initialize game with a blank game board
-gameBoard.renderboard();
-gameBoard.changeGameboard("one", {name: "bob", value: "O"})
+initializeGame();
+
+
